342dafb176cf879c7434d0f4d64f300e5cd2b017
Yaw Osei <Yoannor001@st.ug.edu.gh>
2024-05-18 16:47:22 +0000

    Task 1 & 2 complete

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,3 +1,17 @@
+function processArray(arr) {
+    let result = [];
+    for (let num of arr) {
+        if (num % 2 === 0) { // Even number
+            result.push(num ** 2);
+        } else { // Odd number
+            result.push(num * 3);
+        }
+    }
+    return result;
+}
+
+
+
 function formatArrayStrings(stringsArray, numbersArray) {
     let formattedStrings = [];
     for (let i = 0; i < stringsArray.length; i++) {
