7affe0704ee9200fae0d16995fd043958348f9b0
Yaw Osei <Yoannor001@st.ug.edu.gh>
2024-05-18 16:03:24 +0000

    Task 2 complete

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -1,11 +1,14 @@
-function processArray(arr) {
-    let result = [];
-    for (let num of arr) {
-        if (num % 2 === 0) { // If the number is Even
-            result.push(num ** 2);
-        } else { // If the number is odd
-            result.push(num * 3);
+function formatArrayStrings(stringsArray, numbersArray) {
+    let formattedStrings = [];
+    for (let i = 0; i < stringsArray.length; i++) {
+        let str = stringsArray[i];
+        let num = numbersArray[i];
+
+        if (num % 2 === 0) { // Even number
+            formattedStrings.push(str.toUpperCase());
+        } else { // Odd number
+            formattedStrings.push(str.toLowerCase());
         }
     }
-    return result;
+    return formattedStrings;
 }
